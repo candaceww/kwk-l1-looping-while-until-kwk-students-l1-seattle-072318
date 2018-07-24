022f34a24bbfbe9a1a99f348113cea8e2933f046
candaceww <candacew224@gmail.com>
2018-07-24 22:36:14 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,12 @@
 def using_until
   levitation_force = 6
   
-  
   until levitation_force == 10 
-    puts "Wingardium Leviosa"
-end 
+    puts "Wingardium Leviosa" 
+    levitation_force+= 1
+  end 
+
 
 end 
+using_until
 
